638b3b5f388a8d31ea93c717179d22a0dfd12ca6
Bhanwar <bhanwar.bps89@gmail.com>
2025-01-07 18:18:50 -0500

    some Changes

--- app.py
+++ app.py
@@ -62,22 +62,25 @@ Your the hiring manager for the company and a person who's knows how the ATS sys
 There some Rules and restriction to follow and keep in mind :
 
 1. All ways try using the word cloud of the Job Description.
-2. Please write 3 relevant clear bullet points for workExp
-3. Please write 4 relevant clear bullet points for projectExp.
-4. The points should have some components of Measurable and Quantifiable impact and should be in this format Accomplish X as measured by Y by Doing Z. Please make sure it must be realistic. 
-5. For Project Bullet Points should change the projects that are more relevant to the Job Description and Keep the projects a bit more realistic in way that a single person can do it with in a week.
-6. Bold the most relevant information from the Job description word cloud
-7. Please add 3 Projects.
-8. Add Relevant Skills to the skill section and take the info from the Job Description
-9. For Summery Keep it Clean and relevant to the Job Description
-10. While writing the Projects keep this in mind How write about the Projects :- (What you did and How you did it e.g. Framework, Technology, Tool. Example:- Data Engineering: Build a lager and custom Datasets by implementing fetcher and preprocessing units to periodically **retrieve** data)
-11. DON’T CHANGE INFO SUCH AS Name, number, address, email_link, linkedin_link, Personal_site, github, companyName, location, timeFrame, Education
-12. Keep the Experience 1 year in Summary
-13. there are only 3 section to keep in mind 
+2. Please write 3 relevant clear bullet points for workExp.
+3. While writing points for workExp Keep in mind there are a few users so can use some user matrix if applicable.
+4. Please write four relevant clear bullet points for projectExp. and make it around 1.5 lines per point
+5. While wrting points for projectExp keep in mind there are no users for the Projects.
+6. The points should have some components of Measurable and Quantifiable impact and should be in this format Accomplish X as measured by Y by Doing Z. Please make sure it must be realistic. 
+7. For Project Bullet Points should change the projects that are more relevant to the Job Description and Keep the projects a bit more realistic in way that a single person can do it with in a week.
+8. Bold the most relevant information from the Job description word cloud
+9. Please add 3 Projects.
+10. Add Relevant Skills to the skill section and take the info from the Job Description
+11. For Summery Keep it Clean and relevant to the Job Description
+12. While writing the Projects keep this in mind How write about the Projects :- (What you did and How you did it e.g. Framework, Technology, Tool. Example:- Data Engineering: Build a lager and custom Datasets by implementing fetcher and preprocessing units to periodically **retrieve** data)
+13. DON’T CHANGE INFO SUCH AS Name, number, address, email_link, linkedin_link, Personal_site, github, companyName, location, timeFrame, Education
+14. Keep the Experience 1 year in Summary
+15. Pls add enough information that it will not look and feel Short.
+16. there are only 3 section to keep in mind 
     1. fields (”Summary”, “Tech Skills”)
     2. workExp
     3. projectExp
-14. NEVER CHANGE THIS 
+17. NEVER CHANGE THIS 
         "workExp": (
         "companyName": "WIN Home Inspection",
         "location": "New Delhi, India",
--- genResume.py
+++ genResume.py
@@ -10,9 +10,9 @@ default_fields = {
         "number": "(416)-832-1695",
         "address": "Toronto, ON",
         "email_link": "bhanwar.bps86@gmail.com",
-        "linkedin_link": "linkedin.com/in/bhanwar-singh",
-        "Personal_site" : "bhanwar89.github.io",
-        "github" : "github.com/Bhanwar89",
+        "linkedin_link": "bhanwar-singh",
+        "Personal_site" : "www.neuralbps.com",
+        "github" : "Bhanwar89",
     },
         "education": {
         "s1": {
@@ -35,28 +35,37 @@ default_fields = {
     ]
 }
 
-workinfo_default = {
-        "workExp": {
-        "companyName": "WIN Home Inspection",
-        "location": "New Delhi, India",
-        "jobTitle": "Data Engineer",
-        "timeFrame": "Nov 2019 - Jan 2020",
-        }
-}
 
 class PDF(FPDF):
     def name(self, info):
         self.add_page()
-        self.set_font("helvetica", "B", 19)
+        self.set_font("Arial", "B", 20)
         self.cell(0, 10, info["name"], align="C")
         self.ln(10)
 
     def infoSec(self, info):
-        self.set_font("helvetica", "I", 10)
+        self.set_font("Arial", "I", 10)
         self.cell(10)
-        self.cell(text=f'{info["address"]} | {info["number"]} | ', align="C")
-        self.write_html(f'<a href = " mailto : {info["email_link"]} " > {info["email_link"]} </a> | <a href = " {info["linkedin_link"]} " > {info["linkedin_link"]} </a>')
+        self.cell(text=f'{info["address"]} | {info["number"]} |', align="C")
+        self.write_html(f'''
+        <a href="mailto:{info["email_link"]}">
+        
+        {info["email_link"]}</a> | 
+
+        <a href="https://www.linkedin.com/in/bhanwar-singh/"> 
+
+        {info["linkedin_link"]}</a> | 
+
+        <a href="https://www.neuralbps.com/"> 
+        
+        {info["Personal_site"]}</a> | 
+
+        <a href="https://github.com/Bhanwar89"> 
+        
+        {info['github']}</a>
+        ''')
         self.ln(1)
+
 
     def add_section_line(self):
         self.set_draw_color(169, 169, 169)
@@ -72,54 +81,51 @@ class PDF(FPDF):
 
     def add_html_text(self, text):
         html_text = markdown2.markdown(text)
-        self.set_font("helvetica", "", 10)
+        self.set_font("Arial", "", 10)
         self.write_html(html_text)
         self.ln(3)
 
     def summary(self, fields):
         self.add_section_line()
-        self.set_font("helvetica", "B", 11)
+        self.set_font("Arial", "B", 12)
         self.cell(0, 4, "Summary", ln=True)
         self.add_html_text(fields["Summary"])
 
     def tech_skills(self, fields):
         self.add_section_line()
-        self.set_font("helvetica", "B", 11)
+        self.set_font("Arial", "B", 12)
         self.cell(0, 3, "Technical Skills", ln=True)
         self.add_html_text(fields["Tech Skills"])
 
     def work_experience(self, workExp):
         self.add_section_line()
-        self.set_font("helvetica", "B", 11)
+        self.set_font("Arial", "B", 12)
         self.cell(0, 6, "Work Experience", ln=True)
-        self.set_font("helvetica", "B", 10)
+        self.set_font("Arial", "B", 10)
         self.cell(100, 6, workExp['companyName'], align="L")
         self.cell(90, 6, workExp['location'], align="R", ln=True)
-        self.set_font("helvetica", "I", 10)
+        self.set_font("Arial", "I", 10)
         self.cell(100, 2, workExp['jobTitle'], align="L")
         self.cell(90, 2, workExp['timeFrame'], align="R", ln=True)
         self.add_html_text(workExp['workDescription'])
 
     def project_experience(self, projectExp):
         self.add_section_line()
-        self.set_font("helvetica", "B", 11)
+        self.set_font("Arial", "B", 12)
         self.cell(0, 6, "Project Experience", ln=True)
         for proj in projectExp.values():
-            self.set_font("helvetica", "B", 10)
+            self.set_font("Arial", "B", 11)
             self.cell(0, 4, proj["name"], ln=True)
-            self.set_font("helvetica", "I", 10)
+            self.set_font("Arial", "I", 11)
             self.multi_cell(0, 5,text= f'**Technologies:** {proj["techStack"]}', markdown=True)
-            # self.add_transparent_line()
-            # self.multi_cell(0, 5, text= f'{proj["projectDescription"]}', markdown=True)
-            # self.add_transparent_line()
             self.add_html_text(proj['projectDescription'])
 
     def education(self, education_data):
         self.add_section_line()
-        self.set_font("helvetica", "B", 11)
+        self.set_font("Arial", "B", 12)
         self.cell(0, 6, "Education", ln=True)
         for edu in education_data.values():
-            self.set_font("helvetica", "B", 10)
+            self.set_font("Arial", "B", 10)
             self.cell(100, 6, edu['name'], align="L")
             self.cell(90, 6, edu['location'], align="R", ln=True)
             self.set_font("helvetica", "", 10)
@@ -129,7 +135,7 @@ class PDF(FPDF):
 
     def certificates(self, certificates):
         self.add_section_line()
-        self.set_font("helvetica", "B", 11)
+        self.set_font("Arial", "B", 12)
         self.cell(0, 4, "Certificates", ln=True)
         self.add_html_text('\n'.join([f"- {cert}" for cert in certificates]))
 
